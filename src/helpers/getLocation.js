9c86180c79826c11b15945c98971d4a0628441c3
liubovbutorina <liubovbutorina7@gmail.com>
2024-02-05 02:17:17 +0300

    fix request

--- src/helpers/getLocation.js
+++ src/helpers/getLocation.js
@@ -1,5 +1,5 @@
 export async function getLocation(ip) {
-  const response = await fetch(`http://ipwho.is/${ip}`);
+  const response = await fetch(`http://ipwho.is/${ip}`,  {mode: 'cors'});
 
   return await response.json();
 }
--- src/helpers/getUserIp.js
+++ src/helpers/getUserIp.js
@@ -1,5 +1,12 @@
 export async function getUserIp() {
-  const response = await fetch('http://ipwho.is/');
-  
+  const response = await fetch('http://ipwho.is/', {mode: 'cors'});
+  /*
+  {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Origin:': 'http://ipwho.is/',
+    }
+  });
+  */
   return await response.json();
 }
